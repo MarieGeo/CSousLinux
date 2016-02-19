71c26d49a21cff99e17c086b9ce93d78974035ca
Ambroise <ambroise.thomine@free.fr>
2016-02-19 16:11:19 +0100

    Correction du prog

--- somme.c
+++ somme.c
@@ -2,13 +2,14 @@
 #include <stdarg.h>
 
 int sum(int num,...) {
+	
 	va_list args;
 	int sum = 0, i;
-
 	va_start(args, num);
 
-	for(i = 0, i < num; i++){
-		sum += va_args(args, int);
+	for(i = 0; i < num; i++){
+		//Un S en trop sur la methode
+		sum += va_arg(args, int);
 	}
 
 	va_end(args);
@@ -18,8 +19,8 @@ int sum(int num,...) {
 }
 
 int main(){
-	printf("%d\n", sum(3,4,5));
-	printf("%d\n", sum(5,8,9,6));
+	printf("%d\n", sum(3,4,5, 6));
+	printf("%d\n", sum(5,8,9,6, 5, 6));
 
 	return 0;
 }
