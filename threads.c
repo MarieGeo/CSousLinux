4f2b06e2acd5a92f97c79e4ba8889fb3df75603e
Ambroise <ambroise.thomine@free.fr>
2016-02-22 09:01:24 +0100

    Exo number 2

--- threads.c
+++ threads.c
@@ -7,11 +7,21 @@ void* task_a(void* p_data){
 	return NULL;
 }
 
+void* task_b(void* p_data){
+	printf("Je suis B!\n");
+	return NULL;
+}
+
 int main(){
 	pthread_t main_thread;
+	pthread_t thread_two;
 	int res = pthread_create(&main_thread, NULL, task_a, NULL);
 	printf("res = %d\n", res);
+	res = pthread_create(&thread_two, NULL, task_b, NULL);
+	printf("res = %d\n", res);
+
 
 	pthread_join(main_thread, NULL);
+	pthread_join(thread_two, NULL);
 	return 0;
 }
