e7e41accd651f26938f61f16bd0156ce452a689a
Ambroise <ambroise.thomine@free.fr>
2016-02-22 09:20:04 +0100

    Exo 3, parametres

--- threads.c
+++ threads.c
@@ -3,7 +3,9 @@
 #include <pthread.h>
 
 void* task_a(void* p_data){
+	int* x = (int*) p_data;
 	printf("Je suis A!\n");
+	printf("X vaut %d\n", *x);
 	return NULL;
 }
 
@@ -15,7 +17,8 @@ void* task_b(void* p_data){
 int main(){
 	pthread_t main_thread;
 	pthread_t thread_two;
-	int res = pthread_create(&main_thread, NULL, task_a, NULL);
+	int x = 10;
+	int res = pthread_create(&main_thread, NULL, task_a, &x);
 	printf("res = %d\n", res);
 	res = pthread_create(&thread_two, NULL, task_b, NULL);
 	printf("res = %d\n", res);
